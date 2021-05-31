d4e59a9690f5e503c6b4f9e03101c5538e8a6682
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-05-31 16:25:28 +0900

    Update settings.py

--- myvenv/myapi/settings.py
+++ myvenv/myapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-kf%nz59edk(@kurq^958o%#k+9^0hwn-$b+@2s038y80bb!*)p')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dont see mine,,,,,,,,,,,ToT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
