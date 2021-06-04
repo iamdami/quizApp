c810bedc7bc1b53b21c0fb9e7489917cee6fda3a
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-06-04 10:44:46 +0900

    Add files via upload

---
+++ frontend/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/screen/screen_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyHomePage',
+      home: HomeScreen(),
+    );
+  }
+}
